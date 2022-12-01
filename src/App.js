97d772c08ee1c0ad917b1820963ff042dd4e84f6
Venu Pala <venu.pala@ispace.com>
2022-12-01 18:00:47 +0530

    added a button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+
 function App() {
   const value = 'iSpacian';
   const date = '01DEC22'
-  return <><div>Welcome {value}</div><h1>Today is {date}</h1></>
-  ;
+  return <><div>Welcome {value}</div><h1>Today is {date}</h1>
+  <button onClick={alert("Hello")}>TEST</button></>
 }
 
 export default App;
