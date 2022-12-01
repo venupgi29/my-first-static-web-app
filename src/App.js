d3926a077d21377f726be9d61b5419ce57e2421f
Venu Pala <venu.pala@ispace.com>
2022-12-01 11:57:44 +0530

    changed Date Value

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'iSpacian';
-  const date = date;
+  const date = '01DEC22'
   return <><div>Welcome {value}</div><h1>Today is {date}</h1></>
   ;
 }
