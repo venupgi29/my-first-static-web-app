dc8e229bed9db2d6afdf32572b269453d8faaefd
venupgi29 <32072676+venupgi29@users.noreply.github.com>
2022-12-27 11:24:41 +0530

    ADDED Import for teams

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import * as msTeams from '@microsoft/teams-js';
 
 
 function App() {
+  msTeams.initialize();
   const value = 'iSpacian';
   const date = '28DEC22'
   return <><div>Welcome {value}</div><h1>Today is {date}</h1>
