08b0a87dcab4886c05e03ac026c91e0db51de5b3
venupgi29 <32072676+venupgi29@users.noreply.github.com>
2022-12-27 11:23:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import * as msTeams from '@microsoft/teams-js';
 
 function App() {
   const value = 'iSpacian';
-  const date = '01DEC22'
+  const date = '28DEC22'
   return <><div>Welcome {value}</div><h1>Today is {date}</h1>
-  <button onClick={alert("Hello")}>TEST</button></>
+  <button onClick={alert("Hello")}>HACKATHON IN TEAMS</button></>
 }
 
 export default App;
